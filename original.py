a6589ab3d5c49ec16ebae19a5b4dcce2972ab3d8
mihnea <sofronemihnea@yahoo.com>
2024-06-08 18:10:36 +0300

    "Added two new convolutional layers to the Meso4 class in original.py, and updated several image files and a model file in the configurations directory."

--- original.py
+++ original.py
@@ -179,9 +179,15 @@ class Meso4(Classifier):
         x4 = BatchNormalization()(x4)
         x4 = MaxPooling2D(pool_size=(4, 4), padding='same')(x4)
 
-        
+        x5 = Conv2D(32, (3, 3), padding='same', activation = 'relu')(x4)
+        x5 = BatchNormalization()(x5)
+        x5 = MaxPooling2D(pool_size=(4, 4), padding='same')(x5)
 
-        y = Flatten()(x4)
+        x6 = Conv2D(32, (3, 3), padding='same', activation = 'relu')(x5)
+        x6 = BatchNormalization()(x6)
+        x6 = MaxPooling2D(pool_size=(4, 4), padding='same')(x6)
+
+        y = Flatten()(x6)
         y = Dropout(0.5)(y)
         y = Dense(16)(y)
         y = LeakyReLU(alpha=0.1)(y)
